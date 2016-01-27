589f78158a94903f356cf4c2fdecdc0e36c4861a
nladuo <kalen25115@gmail.com>
2016-01-27 17:30:40 +0800

    3.2.md has been finished

---
+++ code/3.2/HelloActivity/gen/com/example/helloactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.helloactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ code/3.2/HelloActivity/src/com/example/helloactivity/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.helloactivity;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity{
+	
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+	};
+
+}
