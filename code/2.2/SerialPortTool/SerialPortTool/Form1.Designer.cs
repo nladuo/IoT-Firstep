68bdb27334b963397cebfb8aaf79462a7c366d5d
nladuo <kalen25115@gmail.com>
2016-01-29 22:19:18 +0800

    2.2.md has been finished.

---
+++ code/2.2/SerialPortTool/SerialPortTool/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SerialPortTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sendTBox = new System.Windows.Forms.TextBox();
+            this.receivedTBox = new System.Windows.Forms.TextBox();
+            this.baudRateComboBox = new System.Windows.Forms.ComboBox();
+            this.portComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // sendTBox
+            // 
+            this.sendTBox.Location = new System.Drawing.Point(252, 129);
+            this.sendTBox.Multiline = true;
+            this.sendTBox.Name = "sendTBox";
+            this.sendTBox.Size = new System.Drawing.Size(278, 47);
+            this.sendTBox.TabIndex = 2;
+            // 
+            // receivedTBox
+            // 
+            this.receivedTBox.Location = new System.Drawing.Point(252, 12);
+            this.receivedTBox.Multiline = true;
+            this.receivedTBox.Name = "receivedTBox";
+            this.receivedTBox.Size = new System.Drawing.Size(324, 102);
+            this.receivedTBox.TabIndex = 3;
+            // 
+            // baudRateComboBox
+            // 
+            this.baudRateComboBox.FormattingEnabled = true;
+            this.baudRateComboBox.Location = new System.Drawing.Point(72, 94);
+            this.baudRateComboBox.Name = "baudRateComboBox";
+            this.baudRateComboBox.Size = new System.Drawing.Size(96, 20);
+            this.baudRateComboBox.TabIndex = 4;
+            // 
+            // portComboBox
+            // 
+            this.portComboBox.FormattingEnabled = true;
+            this.portComboBox.Location = new System.Drawing.Point(72, 38);
+            this.portComboBox.Name = "portComboBox";
+            this.portComboBox.Size = new System.Drawing.Size(96, 20);
+            this.portComboBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "端口号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "波特率：";
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(12, 146);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 8;
+            this.openBtn.Text = "打开串口";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(111, 146);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 9;
+            this.closeBtn.Text = "关闭串口";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(536, 129);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(40, 47);
+            this.sendBtn.TabIndex = 10;
+            this.sendBtn.Text = "发送";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "接收区：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "发送区：";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 188);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portComboBox);
+            this.Controls.Add(this.baudRateComboBox);
+            this.Controls.Add(this.receivedTBox);
+            this.Controls.Add(this.sendTBox);
+            this.Name = "Form1";
+            this.Text = "SerialPortTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sendTBox;
+        private System.Windows.Forms.TextBox receivedTBox;
+        private System.Windows.Forms.ComboBox baudRateComboBox;
+        private System.Windows.Forms.ComboBox portComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ code/2.2/SerialPortTool2/SerialPortTool/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //取消跨线程检查
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            closeBtn.Enabled = false; //没有打开串口时，无法点击关闭串口
+            sendBtn.Enabled = false;
+            //设置DataReceived的委托
+            serialPort1.DataReceived += serialPort1_DataReceived;
+            //初始化端口号
+            for (int i = 1; i <= 20; i++)
+            {
+                portComboBox.Items.Add("COM" + i);
+            }
+            portComboBox.SelectedIndex = 0;
+
+            //初始化波特率
+            string[] baudRates = {"300","600","1200","2400",
+                                     "4800","9600","19200",
+                                     "38400","43000","56000",
+                                     "57600","115200" };
+            foreach (string baudRate in baudRates)
+            {
+                baudRateComboBox.Items.Add(baudRate);
+            }
+            baudRateComboBox.SelectedIndex = 5;
+            
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string receivedText = serialPort1.ReadExisting();
+                receivedTBox.Text += receivedText;
+                //滚动条滚到底端
+                receivedTBox.SelectionStart = receivedTBox.Text.Length;
+                receivedTBox.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        //打开串口
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = portComboBox.Text;
+                serialPort1.BaudRate = int.Parse(baudRateComboBox.Text);
+                serialPort1.Open();
+                openBtn.Enabled = false;
+                closeBtn.Enabled = true;
+                //打开串口之后波特率和端口号就不能改了
+                baudRateComboBox.Enabled = false;
+                portComboBox.Enabled = false;
+                sendBtn.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //关闭串口
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                openBtn.Enabled = true;
+                closeBtn.Enabled = false;
+
+                baudRateComboBox.Enabled = true;
+                portComboBox.Enabled = true;
+                sendBtn.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //发送数据
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine(sendTBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
