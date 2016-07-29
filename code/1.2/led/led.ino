efc66b1b67a1110b33777b4b379f108511c6e277
Kalen Blue <kalen25115@gmail.com>
2016-07-29 13:00:40 +0800

    finished 1.2.md.

--- code/1.2/led/led.ino
+++ code/1.2/led/led.ino
@@ -1,13 +1,12 @@
-int pin = 13; 
+int pin = 13;               //定义管脚为D13
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(pin, OUTPUT);
+  pinMode(pin, OUTPUT);     //设置D13管脚模式为输出
 }
 
 void loop() {
-  digitalWrite(pin, HIGH);
-  delay(1000);
-  digitalWrite(pin, LOW);
-  delay(1000);
+  //digitalWrite(pin, HIGH);  //在D13管脚输出高电平
+  delay(1000);              //MCU休眠1000ms
+  digitalWrite(pin, LOW);   //在D13管脚输出低电平
+  delay(1000);              //MCU休眠1000ms
 }
