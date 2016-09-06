333f1a744738c015c30b39d7de152b24c424602e
Kalen Blue <kalen25115@gmail.com>
2016-09-06 22:22:00 +0800

    finished 7.1.md.

--- code/7.1/ds18b20/ds18b20.ino
+++ code/7.1/ds18b20/ds18b20.ino
@@ -1,41 +1,28 @@
-// Include the libraries we need
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into port 2 on the Arduino
+// 将DS18B20的数据口连接到2号引脚
 #define ONE_WIRE_BUS 2
-
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+// 初始连接在单总线上的单总线设备
 OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
-/*
- * The setup function. We only start the sensors here
- */
 void setup(void)
 {
-  // start serial port
+  // 串口设置
   Serial.begin(9600);
   Serial.println("Dallas Temperature IC Control Library Demo");
 
-  // Start up the library
+  // 加载库
   sensors.begin();
 }
 
-/*
- * Main function, get and show the temperature
- */
 void loop(void)
 { 
-  // call sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
   Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
+  sensors.requestTemperatures(); // 获取温度
   Serial.println("DONE");
-  // After we got the temperatures, we can print them here.
-  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+ 
   Serial.print("Temperature for the device 1 (index 0) is: ");
   Serial.println(sensors.getTempCByIndex(0));  
 }
