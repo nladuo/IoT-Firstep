4311d3f72aeb3411ad05277a7c588e5a79ff79a8
Kalen Blue <kalen25115@gmail.com>
2017-03-03 00:35:42 +0800

    modified:   book/5.4.md

---
+++ code/5.4/http_server2.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.String(200, "Hello World")
+    })
+    r.Run(":8000")
+}
