03a3fdb2186cbce994d43217d49b56f9c53f3381
Kalen Blue <kalen25115@gmail.com>
2017-03-11 18:18:17 +0800

    finished 7.2.md and updated preface.md.

--- code/7.2/httpClient/httpClient.ino
+++ code/7.2/httpClient/httpClient.ino
@@ -3,7 +3,7 @@
 // ethernet interface mac address, must be unique on the LAN
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
-static uint8_t server_ip[] = { 123, 56, 207, 154 };
+static uint8_t server_ip[] = { 192, 168, 1, 105 };
 byte Ethernet::buffer[700];
 static byte session;
 static uint32_t timer;
@@ -11,51 +11,48 @@ static uint32_t timer;
 static void sendHttpGet () {
   Serial.println("Sending HttpGet...");
 
-  Stash::prepare(PSTR("GET /update HTTP/1.0" "\r\n"
-    "Host: 123.56.207.154:8000 " "\r\n"
+  Stash::prepare(PSTR("GET / HTTP/1.0" "\r\n"
+    "Host:" "\r\n"
     "\r\n"));
 
   session = ether.tcpSend();
-  Serial.println("finished");  
+  Serial.println("finished");
 }
+
 void setup () {
-  Serial.begin(57600);
+  Serial.begin(9600);
   Serial.println("httpGet demo");
 
   if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0) 
     Serial.println("Failed to access Ethernet controller");
   else
     Serial.println("Ethernet controller initialized");
+  
   if (!ether.dhcpSetup())
     Serial.println("DHCP failed");
   else 
     Serial.println("DHCP succeeded");
 
   ether.printIp("IP:  ", ether.myip);
-  ether.printIp("GW:  ", ether.gwip);  
-  ether.printIp("DNS: ", ether.dnsip);  
 
   //设置服务器ip和端口号
   ether.copyIp(ether.hisip, server_ip);
-  ether.hisport = 80;
-
-  
+  ether.hisport = 8000;
 }
 
 void loop () {
-  if (millis() > timer) {
-    timer = millis() + 2000;
+  if (millis() > timer) { // 每隔10秒发一次请求
+    timer = millis() + 10000;
     //发送请求
     sendHttpGet();
   }
 
+  //接受数据包
   ether.packetLoop(ether.packetReceive());
-  //接受请求
   const char* reply = ether.tcpReply(session);
   if (reply != 0) {
     Serial.println("Got a response!");
     Serial.println(reply);
     Serial.println();
   }
-  
 }
---
+++ code/7.2/http_server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+      c.String(200, "Hello World!")
+    })
+
+    r.Run(":8000")
+}
